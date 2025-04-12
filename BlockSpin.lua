206c64ef4e1fbaafb8217ef58440e11fb4ca670d
VoideXlua <voidexlua@gmail.com>
2025-04-12 14:23:40 +0300

    key bug

--- BlockSpin.lua
+++ BlockSpin.lua
@@ -30,7 +30,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
             VoidHubBlockspinFarm = "VoidHubKeyBlockSpin", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
             SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
             GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-            Key = {"D8EY6DV8snucLtYLU37MoHuKCbQv2CKS"}
+            Key = {"D8EY6DV8snucLtYLU37MoHuKCbQv2CKS", "D7Ap5LYuGtUEirVLQwyxCf3EfVNGMcKnSR"}
         }
     })
 
